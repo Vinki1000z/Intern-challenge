3fd4230dc65920840cb5856ae934e10b85e8545a
vinki100z <vikrantchauhancf2021@gmail.com>
2024-07-10 12:31:28 +0530

    adding the navabr

--- unityscore/src/App.js
+++ unityscore/src/App.js
@@ -1,20 +1,21 @@
 // App.js
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import GoogleLogin from './Components/GoogleLogin';
-import Dashboard from './Components/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
+// importing the css file
+import "./App.css"
+// import GoogleLogin from "./Components/GoogleLogin";
+// import Dashboard from "./Components/Dashboard";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <Router>
-          <Routes>
-       <Route path="/" element={<GoogleLogin/>} />        
-       <Route path="/dashboard" element={<Dashboard/>} />        
-       </Routes>
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+      </Routes>
     </Router>
   );
 }
-
 
 export default App;
